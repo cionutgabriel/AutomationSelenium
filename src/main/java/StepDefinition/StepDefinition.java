9bcd47f008c030339d308b42683db86aa449c4ec
Ionut C <cionutgabriel91@gmail.com>
2023-08-08 21:18:53 +0300

    open browser

--- src/main/java/StepDefinition/StepDefinition.java
+++ src/main/java/StepDefinition/StepDefinition.java
@@ -1,4 +1,19 @@
 package StepDefinition;
+import Utilities.ChromeConfig;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class StepDefinition {
+	ChromeDriver driver;
+
+	@Given("Open the browser")
+	public void deschideChrome(){
+		driver=ChromeConfig.getChromeDriver();
+	}
+	@When("Access page {string}")
+	public void acceseazaPagina(String url){
+		driver.get(url);
+	}
+
 }
